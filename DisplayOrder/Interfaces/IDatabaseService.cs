a6d189bba5e94b305fab7f59705e29b7b13f8d16
MixterKill29 <mixter.kill29@gmail.com>
2023-09-11 09:25:47 +0200

    fix connection close

--- DisplayOrder/Interfaces/IDatabaseService.cs
+++ DisplayOrder/Interfaces/IDatabaseService.cs
@@ -8,7 +8,7 @@ namespace DisplayOrder.Interfaces
         List<OrderModel> GetOrdersDB(string language);
         int PostOrdersDB(POST_OrderModel order);
         void PostOrdersDB(POST_OrderModel order, string orderNumber);
-        OrderModel UpdateOrderDB(UpdateRequestModel update, string language);
+        void UpdateOrderDB(UpdateRequestModel update, string language);
 
     }
 }
--- DisplayOrder/Services/DatabaseService.cs
+++ DisplayOrder/Services/DatabaseService.cs
@@ -15,33 +15,41 @@ namespace DisplayOrder.Services
             _configuration = configuration;
         }
 
-        public OrderModel UpdateOrderDB(UpdateRequestModel update, string language)
+        public void UpdateOrderDB(UpdateRequestModel update, string language)
         {
-            con = new SqlConnection(_configuration.GetSection("appsettings").GetValue<string>("connectionstring"));
-
-            con.Open();
-            string query = @$"UPDATE [dbo].[Diplay_Order]
+            using (con = new SqlConnection(_configuration.GetSection("appsettings").GetValue<string>("connectionstring")))
+            {
+                try
+                {
+                    con.Open();
+                    string query = @$"UPDATE [dbo].[Diplay_Order]
                                  SET 
                                     [order_status] = {update.order_status},
                                     [Update_date] = GetDate()
                                     WHERE [order_id] = {update.order_id}";
-            using (SqlCommand cmd = new SqlCommand(query, con))
+                    using (SqlCommand cmd = new SqlCommand(query, con))
 
-            {
-                cmd.ExecuteNonQuery();
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                finally
+                {
+                    con.Close();
+                }
+
             }
-            con.Close();
-            return null;
         }
         public List<OrderModel> GetOrdersDB(string language)
         {
+            using (con = new SqlConnection(_configuration.GetSection("appsettings").GetValue<string>("connectionstring")))
+            {
+                List<OrderModel> result = new List<OrderModel>();
+                try
+                {
+                    con.Open();
 
-
-            con = new SqlConnection(_configuration.GetSection("appsettings").GetValue<string>("connectionstring"));
-
-            con.Open();
-            List<OrderModel> result = new List<OrderModel>();
-            string query = @$"SELECT 
+                    string query = @$"SELECT 
                                 [order_id],
                                 [Json_Order],
                                 [Order_Number],
@@ -58,102 +66,113 @@ namespace DisplayOrder.Services
                             WHERE CONVERT(date, D.[insert_date]) = CONVERT(date, GETDATE()) 
                                 AND D.[order_status] < 4
                             order by d.Insert_date";
-            using (SqlCommand cmd = new SqlCommand(query, con))
+                    using (SqlCommand cmd = new SqlCommand(query, con))
 
-            {
-
-                using (SqlDataReader reader = cmd.ExecuteReader())
-                {
-                    if (!reader.HasRows)
-                    {
-                        con.Close();
-                        return result;
-                    }
-
-                    while (reader.Read())
-                    {
-                        result.Add(new OrderModel(reader["order_id"].ToString()
-                            , reader["Order_Number"].ToString(),
-                            JsonConvert.DeserializeObject<List<ItemModel>>(reader["Json_Order"].ToString())
-                            , int.Parse(reader["order_status"].ToString()),
-                            reader.GetDateTime("Insert_date").ToString("dd/MM/yyyy HH:mm:ss"),
-                            int.Parse(reader["result_DateMinutes"].ToString()),
-                            int.Parse(reader["result_DateSeconds"].ToString()),
-                            reader["Img"].ToString(),
-                            reader["EmployeeId"].ToString(),
-                            reader["EmployeeName"].ToString()
-                            ));
-                    }
-                }
-
-                List<string> itemIds = new List<string>();
-                result.ForEach(order =>
-                {
-                    order.items.Where(item => item.name == null).ToList().ForEach(item =>
                     {
 
-                        itemIds.Add(item.id.ToString());
-                        item.option.ForEach(option => itemIds.Add(option.id.ToString()));
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (!reader.HasRows)
+                            {
+                                con.Close();
+                                return result;
+                            }
 
-                    });
-                });
-                if (itemIds.Count > 0)
-                {
-                    cmd.CommandText = $@"select i.id,isnull(it.Value,i.Name) name
+                            while (reader.Read())
+                            {
+                                result.Add(new OrderModel(reader["order_id"].ToString()
+                                    , reader["Order_Number"].ToString(),
+                                    JsonConvert.DeserializeObject<List<ItemModel>>(reader["Json_Order"].ToString())
+                                    , int.Parse(reader["order_status"].ToString()),
+                                    reader.GetDateTime("Insert_date").ToString("dd/MM/yyyy HH:mm:ss"),
+                                    int.Parse(reader["result_DateMinutes"].ToString()),
+                                    int.Parse(reader["result_DateSeconds"].ToString()),
+                                    reader["Img"].ToString(),
+                                    reader["EmployeeId"].ToString(),
+                                    reader["EmployeeName"].ToString()
+                                    ));
+                            }
+                        }
+
+                        List<string> itemIds = new List<string>();
+                        result.ForEach(order =>
+                        {
+                            order.items.Where(item => item.name == null).ToList().ForEach(item =>
+                            {
+
+                                itemIds.Add(item.id.ToString());
+                                item.option.ForEach(option => itemIds.Add(option.id.ToString()));
+
+                            });
+                        });
+                        if (itemIds.Count > 0)
+                        {
+                            cmd.CommandText = $@"select i.id,isnull(it.Value,i.Name) name
                             from [TPKioskDB].dbo.RestaurantItem i
                             left join [TPKioskDB].dbo.RestaurantItemTranslation it on i.ID = it.ID_Item
 	                            and it.Language = '{language}' and it.Field ='Name' 
                             where i.ID in({string.Join(",", itemIds)})";
 
-                    Dictionary<string, string> map = new Dictionary<string, string>();
+                            Dictionary<string, string> map = new Dictionary<string, string>();
 
-                    using (SqlDataReader reader = cmd.ExecuteReader())
-                    {
-
-
-                        while (reader.Read())
-                        {
-                            map.Add(reader["id"].ToString(), reader["name"].ToString());
-                        }
-                    }
-                    result.ForEach(order =>
-                    {
-                        order.items.ForEach(item =>
-                        {
-                            if (item.name == null)
+                            using (SqlDataReader reader = cmd.ExecuteReader())
                             {
-                                if (map.ContainsKey(item.id.ToString()))
+
+
+                                while (reader.Read())
                                 {
-                                    item.name = map[item.id.ToString()];
+                                    map.Add(reader["id"].ToString(), reader["name"].ToString());
                                 }
                             }
-                            item.option.ForEach(option =>
+                            result.ForEach(order =>
                             {
-                                if (option.name == null)
+                                order.items.ForEach(item =>
                                 {
-                                    if (map.ContainsKey(option.id.ToString()))
+                                    if (item.name == null)
                                     {
-                                        option.name = map[option.id.ToString()];
+                                        if (map.ContainsKey(item.id.ToString()))
+                                        {
+                                            item.name = map[item.id.ToString()];
+                                        }
                                     }
-                                }
+                                    item.option.ForEach(option =>
+                                    {
+                                        if (option.name == null)
+                                        {
+                                            if (map.ContainsKey(option.id.ToString()))
+                                            {
+                                                option.name = map[option.id.ToString()];
+                                            }
+                                        }
+                                    });
+                                });
                             });
-                        });
-                    });
+                        }
+
+
+                    }
                 }
-
-
+                finally
+                {
+                    con.Close();
+                }
+                return result;
             }
-            con.Close();
-            return result;
+
         }
 
         public int PostOrdersDB(POST_OrderModel order)  // funzione chiamata dal chiosco per scrivere gli ordini sul db del display
         {
-            con = new SqlConnection(_configuration.GetSection("appsettings").GetValue<string>("connectionstring"));
-            con.Open();
-            int result;
-            string query1 = @$"SELECT NEXT VALUE For [dbo].[Display_OrderSequence] as OrderNumberKiosk";
-            string query2 = @$"INSERT INTO [dbo].[Diplay_Order](
+            using (con = new SqlConnection(_configuration.GetSection("appsettings").GetValue<string>("connectionstring")))
+            {
+                int result;
+
+                try
+                {
+                    con.Open();
+
+                    string query1 = @$"SELECT NEXT VALUE For [dbo].[Display_OrderSequence] as OrderNumberKiosk";
+                    string query2 = @$"INSERT INTO [dbo].[Diplay_Order](
                                 [Json_Order],
                                 [Order_Number],
 		                        [order_status],
@@ -164,56 +183,67 @@ namespace DisplayOrder.Services
 
 
 
-            using (SqlCommand cmd = new SqlCommand(query1, con))
+                    using (SqlCommand cmd = new SqlCommand(query1, con))
 
-            {
-
-
-                using (SqlDataReader reader = cmd.ExecuteReader())
-                {
-                    if (reader.HasRows)
                     {
+
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+
+                            }
+
+                            if (reader.Read())
+                            {
+
+                                result = int.Parse(reader["OrderNumberKiosk"].ToString()!);
+                                //query2 = @$"INSERT INTO [dbo].[Diplay_Order](
+
+                                //        [Json_Order],
+                                //        [Order_Number],
+                                //  [order_status],
+                                //        [Cod_Consumation])
+                                //        SELECT N'{JsonConvert.SerializeObject(order.order)}', {result},1,'{order.Cod_Consumation}'";
+
+                            }
+                            else
+                            {
+                                throw new ArgumentException();
+                            }
+
+                        }
+
+
+                        cmd.Parameters.AddWithValue("@orderNumber", result);
+                        cmd.Parameters.AddWithValue("@id", order.kioskId);
+
+                        cmd.CommandText = query2;
+                        cmd.ExecuteNonQuery();
 
                     }
 
-                    if (reader.Read())
-                    {
 
-                        result = int.Parse(reader["OrderNumberKiosk"].ToString()!);
-                        //query2 = @$"INSERT INTO [dbo].[Diplay_Order](
-
-                        //        [Json_Order],
-                        //        [Order_Number],
-                        //  [order_status],
-                        //        [Cod_Consumation])
-                        //        SELECT N'{JsonConvert.SerializeObject(order.order)}', {result},1,'{order.Cod_Consumation}'";
-
-                    }
-                    else
-                    {
-                        throw new ArgumentException();
-                    }
 
                 }
-
-
-                cmd.Parameters.AddWithValue("@orderNumber", result);
-                cmd.Parameters.AddWithValue("@id", order.kioskId);
-
-                cmd.CommandText = query2;
-                cmd.ExecuteNonQuery();
-
+                finally
+                {
+                    con.Close();
+                }
+                return result;
             }
-            con.Close();
-            return result;
         }
 
 
         public void PostOrdersDB(POST_OrderModel order, string orderNumber)  // funzione chiamata dal nav per scrivere gli ordini sul db del display
         {
-            con = new SqlConnection(_configuration.GetSection("appsettings").GetValue<string>("connectionstring"));
-            con.Open();
-            string query = @$"INSERT INTO [dbo].[Diplay_Order](
+            using (con = new SqlConnection(_configuration.GetSection("appsettings").GetValue<string>("connectionstring")))
+            {
+                try
+                {
+                    con.Open();
+                    string query = @$"INSERT INTO [dbo].[Diplay_Order](
                                 [Json_Order],
                                 [Order_Number],
 		                        [order_status],
@@ -221,17 +251,22 @@ namespace DisplayOrder.Services
                                 [EmployeeId],
                                 [EmployeeName])
                                 SELECT @json, @orderNumber,1,'{order.Cod_Consumation}',@id,'POS'";
-            using (SqlCommand cmd = new SqlCommand(query, con))
-            {
-                cmd.Parameters.AddWithValue("@orderNumber", orderNumber);
-                cmd.Parameters.AddWithValue("@id", order.kioskId);
-                cmd.Parameters.AddWithValue("@json", JsonConvert.SerializeObject(order.order));
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@orderNumber", orderNumber);
+                        cmd.Parameters.AddWithValue("@id", order.kioskId);
+                        cmd.Parameters.AddWithValue("@json", JsonConvert.SerializeObject(order.order));
 
-                int rowcount = cmd.ExecuteNonQuery();
-                if (rowcount == 0) { con.Close(); throw new Exception("Couldn't insert the order!"); }
+                        int rowcount = cmd.ExecuteNonQuery();
+                        if (rowcount == 0) { con.Close(); throw new Exception("Couldn't insert the order!"); }
 
+                    }
+                }
+                finally
+                {
+                    con.Close();
+                }
             }
-            con.Close();
         }
 
         public void SendOrderToNav(POST_OrderModel orderModel, string orderNumber)
